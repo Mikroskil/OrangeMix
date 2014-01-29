4b6c9eeaedd3de01921aad3c39ce0f6961416a59
Yogie Pratama <yogiepratamas@yahoo.co.id>
2014-01-29 15:04:22 +0700

    Create tabah_kategori1.php

---
+++ tabah_kategori1.php
@@ -0,0 +1,7 @@
+<?php require_once 'functions.php';connect() ?>
+<?php
+if (isset( $_SERVER['HTTP_X_REQUESTED_WITH'] )):
+	$id_barang = $_POST['id_barang'];
+	$nama_barang = $_POST['nama_barang'];
+	$query = mysql_query("INSERT INTO barang VALUES ('','$id_barang','$nama_barang')");
+endif?>
